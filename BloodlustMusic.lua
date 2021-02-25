acdc2453c26b531310db88564f8d4f1f460bd78a
jordodo <jordi.bottema@gmail.com>
2021-02-25 23:55:53 +0100

    Initial (D) commit

---
+++ BloodlustMusic.lua
@@ -0,0 +1,569 @@
+BloodlustMusic = { };
+
+BloodlustMusic.songPathTable = {
+    [[GasHero.ogg]],        --1
+    [[90sHero.ogg]],        --2
+    [[BrainPowerHero.ogg]], --3
+    [[BirdHero.ogg]],       --4
+    [[SandHero.ogg]],       --5
+    [[NightHero.ogg]],      --6
+    [[ElephantHero.ogg]],   --7
+    [[SaiyanHero.ogg]],     --8
+    [[JellyfishHero.ogg]],  --9
+    [[TopHero.ogg]],        --10
+    [[DDDHero.ogg]],        --11
+    [[Up&DanceHero.ogg]],   --12
+    [[HardcoreHero.ogg]],   --13
+    [[PerfectHero.ogg]],    --14
+    [[SpeedyHero.ogg]],     --15
+    [[CascadaHero.ogg]],    --16
+    [[SunRainHero.ogg]],    --17
+    [[DontStopHero.ogg]],   --18
+    [[InvadersHero.ogg]],   --19
+    [[CallOnMeHero.ogg]],   --20
+    [[KissKillHero.ogg]],   --21
+    [[LookaHero.ogg]],      --22
+    [[AnchorHero.ogg]],     --23
+    [[FullMetalHero.ogg]],  --24
+    [[KickstartHero.ogg]],  --25
+    [[RiderHero.ogg]],      --26
+    [[RisingSunHero.ogg]],  --27
+    [[BloodSugarHero.ogg]], --28
+    [[DejaVuHero.ogg]],     --29
+    [[RumbleHero.ogg]],     --30
+    [[HurricaneHero.ogg]],  --31
+    [[DontTurnHero1.ogg]],  --32
+    [[DontTurnHero2.ogg]],  --33
+    [[EuronightHero.ogg]],  --34
+    [[BeatinWildHero.ogg]], --35
+    [[KingHero.ogg]],       --36
+    [[SuperRiderHero.ogg]], --37
+    [[PistonGoHero.ogg]],   --38
+    [[DancingHero.ogg]],    --39
+    [[BeatCrazyHero.ogg]],  --40
+    [[SpitfireHero.ogg]],   --41
+    [[ShockOutHero.ogg]],   --42
+    [[SpeedLoverHero.ogg]], --43
+	[[ShrekHero.ogg]],      --44
+	[[BonkersHero.ogg]],    --45
+    [[HyperSuperHero.ogg]], --46
+    [[SuperstarHero.ogg]]   --47
+}
+
+BloodlustMusic.songNameTable = {
+    [[Manuel - GAS GAS GAS]],
+    [[Max Coveri - Running in the 90's]],
+    [[Noma - Brainpower]],
+    [[Lynyrd Skynyrd - Freebird]],
+    [[Darude - Sandstorm]],
+    [[Niko - Night of Fire]],
+    [[The Elephant Rave]],
+    [[Fartwad - Stereo Saiyan 3D]],
+    [[The Jellyfish Jam]],
+    [[Ken Blast - The Top]],
+    [[Mega NRG Man - DDD Initial D]],
+    [[Lou Master - Up & Dance, Up & Go]],
+    [[Fastway - Rockin' Hardcore]],
+    [[Chris Stanton - A Perfect Hero]],
+    [[Marco Polo - Speedy Speed Boy]],
+    [[Cascada - Everytime we Touch]],
+    [[Manuel - Sun In The Rain]],
+    [[Lou Grant - Don't Stop The Music]],
+    [[The Prodigy - Invaders must die]],
+    [[Eric Prydz - Call On Me]],
+    [[Odyssey - Kiss Me Kill Me]],
+    [[Go2 - Looka Bomba]],
+    [[ALESTORM - Fucked With An Anchor]],
+    [[Daniel - Full Metal Cars]],
+    [[Mötley Crüe - Kickstart my Heart]],
+    [[Ace - Rider Of The Sky]],
+    [[Dave Rodgers - Beat Of The Rising Sun]],
+    [[Pendulum - Blood Sugar]],
+    [[Dave Rodgers - Deja Vu]],
+    [[Jock Jams - Let's Get Ready To Rumble]],
+    [[Gold-Rake - Hurricane Man]],
+    [[Go2 - Don't Turn It Off (Chorus)]],
+    [[Go2 - Don't Turn It Off (Solo)]],
+    [[Eurogroove - Euronight]],
+    [[Fastway - Rock Beatin' Wild]],
+    [[Jordan - King Of Eurobeat]],
+    [[Mark Astley - Super Rider]],
+    [[Gordon Jim - Piston Go]],
+    [[Vicky Vale - Dancing]],
+    [[Fastway - Go Beat Crazy]],
+    [[Go2 - Spitfire]],
+    [[Fastway - Shock Out]],
+    [[Speedman - Speed Lover]],
+	[[Jennifer Saunders - Holding Out for a Hero]],
+	[[Dizzee Rascal & Armand van Helden - Bonkers]],
+    [[Fastway - Hyper Super Power]],
+    [[Go2 & DJ Boss - Superstar]]
+}
+
+--[[
+BloodlustMusicSongEnabledTable = {
+    true, --gas
+    true, --nineties
+    true, --brain
+    true, --freebird
+    true, --sandstorm = true,
+    true, --night = true,
+    true, --elephant = true,
+    true, --saiyan = true,
+    true, --jellyfish = true,
+    true, --thetop = true,
+    true, --ddd = true,
+    true, --updance = true,
+    true, --hardcore = true,
+    true, --perfect = true,
+    true, --speedy = true,
+    true, --cascada = true,
+    true, --sunrain = true,
+    true, --dontstop = true,
+    true, --invaders = true,
+    true, --callonme = true,
+    true, --kisskill = true,
+    true, --looka = true,
+    true, --anchor = true,
+    true, --fullmetal = true,
+    true, --kickstart = true,
+    true, --rider = true,
+    true, --risingsun = true,
+    true, --bloodsugar = true,
+    true, --dejavu = true,
+    true, --rumble = true,
+    true, --hurricane = true,
+    true, --dontturn1 = true,
+    true, --dontturn2 = true,
+    true, --euronight = true,
+    true, --beatinwild = true,
+    true, --king = true,
+    true, --superrider = true,
+    true, --pistongo = true,
+    true, --dancing = true,
+    true, --beatcrazy = true,
+    true, --spitfire = true,
+    true, --shockout = true,
+    true, --speedlover = true
+	true, --shrek = true
+	true, --bonkers = true
+    true, --hypersuper = true
+    true  --superstar = true
+}
+--]]
+
+BloodlustMusic.soundChannelTable = {
+    "master",
+    "sfx",
+    "music",
+    "ambience",
+    "dialog"
+}
+BloodlustMusic.soundVolumeTable = {
+    "Sound_MasterVolume",
+    "Sound_SFXVolume",
+    "Sound_MusicVolume",
+    "Sound_AmbienceVolume",
+    "Sound_DialogVolume"
+
+
+}
+BloodlustMusic.soundChannelNames = {
+    "Master",
+    "SFX",
+    "Music",
+    "Ambience",
+    "Dialog"
+}
+
+
+SLASH_BLOODLUSTMUSIC1, SLASH_BLOODLUSTMUSIC2 = '/blm', '/bloodlust';
+
+function SlashCmdList.BLOODLUSTMUSIC(msg, editbox)
+	--InterfaceOptionsFrame_OpenToCategory(BloodlustMusic.panel);
+	--InterfaceOptionsFrame_OpenToCategory(BloodlustMusic.panel);
+	InterfaceOptionsFrame:Show()
+	InterfaceOptionsFrame_OpenToCategory(BloodlustMusic.panel);
+	--BloodlustMusic.panel:Show();
+end
+
+--Declaring Variables
+local f = CreateFrame("Frame");
+local playerGUID;
+local tried = 0
+local randomNumber = 0
+local willPlay = 0
+local defaultFilePath = "interface/addons/bloodlustmusic/sounds/"
+local currentFilePath = " "
+local currentlyPlaying = " "
+local minute = 0
+local songNumber = 0
+local soundchannelscache = GetCVar("Sound_NumChannels")
+local spellIDS = {80353, 32182, 2825, 264667, 146555, 178207, 256740, 230935, 309658}
+local isSongPlaying = false
+local currentSongSpellID
+
+C_Timer.After(.1, function() -- wait a bit
+	playerGUID = UnitGUID("player");
+end)
+
+
+
+function PlaySong(song)
+    currentFilePath = defaultFilePath;
+	currentlyPlaying = "Now Playing: "
+	print("inside PlaySong")
+
+
+    if(BloodlustMusicSongEnabledTable[song])
+    then
+        currentlyPlaying = currentlyPlaying .. BloodlustMusic.songNameTable[song]
+
+        currentFilePath = currentFilePath .. BloodlustMusic.songPathTable[song]
+    else
+        currentlyPlaying = currentlyPlaying .. "No Song"
+
+        currentFilePath = currentFilePath .. "NoSong.ogg"
+	end
+
+	willPlay, BloodlustSoundhandle = PlaySoundFile(currentFilePath, BloodlustMusic.soundChannelTable[BloodlustSoundchannelNumber])
+	--print(willPlay)
+	--print(BloodlustSoundhandle)
+
+	--print(BloodlustSoundhandle .. " inside song")
+
+end
+
+function SongPlayer(heroSpellID)
+	if (not isSongPlaying) then
+	--Resetting some variables
+	tried = 0
+	randomNumber = 0
+	defaultFilePath = "interface/addons/bloodlustmusic/sounds/"
+	currentFilePath = " "
+	currentlyPlaying = " "
+	BloodlustVolumecache = tonumber(GetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber]))
+  	soundchannelscache = GetCVar("Sound_NumChannels")
+
+	--gets the current local time (minute)
+	minute = (date("%M"))
+
+
+	--makes sure songNumber is never zero and makes sure songNumber can't be higher than 47. Lua tables start at 1 and there are only 47 songs
+	songNumber = minute + 1
+	if (songNumber > 47)
+	then
+		songNumber = songNumber - 13
+	end
+
+	--plays the song
+	BloodlustVolumecache = tonumber(GetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber]))
+  	soundchannelscache = GetCVar("Sound_NumChannels")
+
+	SetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber], BloodlustVolumecache < BloodlustChannelVolume and BloodlustChannelVolume or BloodlustVolumecache)
+	--SetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber],  BloodlustChannelVolume)
+  	SetCVar("Sound_NumChannels", 128)
+
+	PlaySong(songNumber)
+	--print("After PlaySong ")
+
+	--checks if song actually played
+	if(not willPlay == true)
+	then
+		--if song didn't play, a random one is selected below
+		repeat
+
+			--generates a random number
+			randomNumber=math.random(1,47)
+			print("RNG " .. randomNumber)
+
+			--plays the song
+			PlaySong(randomNumber)
+			tried = tried + 1
+
+			--ends the loop if a song played or after 20 tries
+		until((not willPlay == false) or (tried >= 20))
+	end
+
+	--displays the current song playing, or that it failed to play any
+	if(tried >= 20)
+	then
+		print("No song was selected. Please check your Addon or Sound settings")
+	else
+		isSongPlaying = true
+		currentSongSpellID = heroSpellID
+		print(currentlyPlaying)
+	end
+else 
+	print("A song is already playing.")
+end
+end
+
+function StopSong()
+	--Stops the song when Hero ends or is cancelled
+	isSongPlaying = false
+	currentSongSpellID = 0
+	print(BloodlustSoundhandle.. " stop song func")
+	StopSound(BloodlustSoundhandle)
+	SetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber], BloodlustVolumecache)
+  	SetCVar("Sound_NumChannels", soundchannelscache)
+	print("Sound Stopped")
+end
+
+function f:OnEvent()
+	local _, event, _, _, _, _, _, destinationGUID, _, _, _, spellID, spellName, _, _ = CombatLogGetCurrentEventInfo();
+	--print(event); --event type
+	--print(destinationGUID);
+	--print(spellID .. " " .. spellName)
+
+		if (event == "SPELL_AURA_APPLIED" and destinationGUID == playerGUID)
+		then
+			--print("Buff applied: " .. spellID .. " " .. spellName)
+			for key,value in pairs(spellIDS) do
+				if (value == spellID)
+				then
+				--print("spellID match applied");
+				SongPlayer(value);
+				end
+			end
+		elseif (event == "SPELL_AURA_REMOVED" and destinationGUID == playerGUID)
+		then
+			--print("Buff Removed: " .. spellID .. " " .. spellName)
+			for key,value in pairs(spellIDS) do
+				if (value == spellID and value == currentSongSpellID)
+				then
+				
+				--print(spellID)
+				--print(currentSongSpellID)	
+				--print("spellID match removed");
+				StopSong();
+				
+			end
+			end
+		end
+end
+
+
+
+--f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+f:SetScript("OnEvent", f.OnEvent);
+
+--Main Panel
+BloodlustMusic.panel = CreateFrame( "Frame", "BloodlustMusicPanel", UIParent );
+BloodlustMusic.panel.name = "BloodlustMusic";
+InterfaceOptions_AddCategory(BloodlustMusic.panel);
+
+ -- Options Panel
+ BloodlustMusic.optionspanel = CreateFrame( "Frame", "BloodlustMusicSongPanel", BloodlustMusic.panel);
+ BloodlustMusic.optionspanel.name = "Options";
+ BloodlustMusic.optionspanel.parent = BloodlustMusic.panel.name;
+ InterfaceOptions_AddCategory(BloodlustMusic.optionspanel);
+
+-- Song List Panel
+ BloodlustMusic.songpanel = CreateFrame( "Frame", "BloodlustMusicSongPanel", BloodlustMusic.panel);
+ BloodlustMusic.songpanel.name = "Song List";
+ BloodlustMusic.songpanel.parent = BloodlustMusic.panel.name;
+ InterfaceOptions_AddCategory(BloodlustMusic.songpanel);
+
+
+
+function BloodlustMusic.okay()
+	xpcall(function()
+		print("okay")
+	end, geterrorhandler())
+end
+
+
+
+local function PanelCreation()
+	--Example:
+	print("Inside PanelCreation")
+	local button = CreateFrame("Button","MyExampleButton", BloodlustMusic.optionspanel,"UIPanelButtonTemplate") --frameType, frameName, frameParent, frameTemplate
+	button:SetSize(80,40)
+	button:SetPoint("TOPLEFT",10,-10)
+	button.text = _G["MyExampleButton".."Text"]
+	button.text:SetText("Print")
+	button:SetScript("OnClick", function(self, arg1)
+		print("Print Button is pressed");
+		--getglobal("TestCheckButton"):SetChecked(true);
+		print(BloodlustMusicSongEnabledTable[1]);
+		print(BloodlustMusicSongEnabledTable[2]);
+		print(BloodlustMusicSongEnabledTable[3]);
+		print("Volume during Hero should be: " .. BloodlustChannelVolume)
+		print("Normal Volume level should be: " .. BloodlustVolumecache)
+		print("isSongPlaying  = " .. tostring(isSongPlaying))
+		print("currentSongSpellID = " .. tostring(currentSongSpellID))
+	end)
+
+
+	local BloodlustSlider = CreateFrame("Slider", "BloodlustSliderGlobalName", BloodlustMusic.optionspanel, "OptionsSliderTemplate")
+	BloodlustSlider:SetPoint("TOPRIGHT",-100,-100)
+	BloodlustSlider:SetMinMaxValues(0, 1)
+    BloodlustSlider:SetValueStep(0.1)
+	BloodlustSlider:SetOrientation('HORIZONTAL')
+	BloodlustSlider.text = _G[BloodlustSlider:GetName() .. "Text"]
+    BloodlustSlider.low = _G[BloodlustSlider:GetName() .. "Low"]
+    BloodlustSlider.high = _G[BloodlustSlider:GetName() .. "High"]
+
+    BloodlustSlider.text:SetText(math.floor(BloodlustChannelVolume*100)/100)
+    BloodlustSlider.low:SetText("0")
+    BloodlustSlider.high:SetText("1")
+	BloodlustSlider.tooltipText = "This is the tooltip"
+	--BloodlustSlider:SetThumbTexture(_ ["HIGH"])
+	BloodlustSlider:SetScript("OnValueChanged", function(self,event,arg1)
+		BloodlustChannelVolume = math.floor(BloodlustSlider:GetValue()*100)/100
+		BloodlustSlider.text:SetText(math.floor(BloodlustChannelVolume*100)/100)
+	end)
+	print("create slider")
+	BloodlustSlider:Show()
+
+	-- Create the dropdown, and configure its appearance
+	local dropDown = CreateFrame("FRAME", "WPDemoDropDown", BloodlustMusic.optionspanel, "UIDropDownMenuTemplate")
+	dropDown:SetPoint("CENTER")
+	UIDropDownMenu_SetWidth(dropDown, 200)
+	UIDropDownMenu_SetText(dropDown, "Current soundchannel: " .. BloodlustMusic.soundChannelNames[BloodlustSoundchannelNumber])
+
+	-- Create and bind the initialization function to the dropdown menu
+	UIDropDownMenu_Initialize(dropDown, function(self, level, menuList)
+		local info = UIDropDownMenu_CreateInfo()
+		if (level or 1) == 1 then
+			-- Display the 0-9, 10-19, ... groups
+			for a, c in ipairs(BloodlustMusic.soundChannelTable) do
+				info.text = BloodlustMusic.soundChannelNames[a]
+				info.func = self.SetValue
+				--info.menuList, info.hasArrow = a, true
+				info.arg1, info.checked = a, a == BloodlustSoundchannelNumber
+				UIDropDownMenu_AddButton(info)
+			end
+		end
+	end)
+
+	-- Implement the function to change the favoriteNumber
+	function dropDown:SetValue(newValue)
+		BloodlustSoundchannelNumber = newValue
+		-- Update the text; if we merely wanted it to display newValue, we would not need to do this
+		UIDropDownMenu_SetText(dropDown, "Current soundchannel: " .. BloodlustMusic.soundChannelNames[BloodlustSoundchannelNumber])
+		-- Because this is called from a sub-menu, only that menu level is closed by default.
+		-- Close the entire menu with this next call
+		CloseDropDownMenus()
+	end
+
+	--Create Checkbuttons
+	for a,c in ipairs(BloodlustMusicSongEnabledTable) do
+		-- create checkbox
+		--print("inside forloop")
+			if (a < 31) then
+				--print("a < 21")
+				local checkbox = CreateFrame("CheckButton", "TestCheckButton ".. a, BloodlustMusic.songpanel, "UICheckButtonTemplate")
+				checkbox:SetPoint("TOPLEFT", BloodlustMusic.songpanel, "TOPLEFT", 5, ((a-1)* -18.6))
+				checkbox:SetSize(27, 27)
+				checkbox.text:SetFontObject("GameFontNormal")
+				checkbox.text:SetText(BloodlustMusic.songNameTable[a])
+				checkbox.text:SetTextColor(1, 1, 1, 1)
+
+				if (getglobal("TestCheckButton "..a):GetChecked() ~= BloodlustMusicSongEnabledTable[a]) then
+					--print("inside getglobal");
+					getglobal("TestCheckButton "..a):SetChecked(BloodlustMusicSongEnabledTable[a]);
+				end
+
+				checkbox:SetScript("OnClick", function(self,event,arg1)
+					if (self:GetChecked()) then
+						--print("set to true");
+						BloodlustMusicSongEnabledTable[a] = true;
+					else
+						--print("set to false");
+						BloodlustMusicSongEnabledTable[a] = false;
+					end
+				end)
+			else
+				local checkbox = CreateFrame("CheckButton", "TestCheckButton ".. a, BloodlustMusic.songpanel, "UICheckButtonTemplate")
+				checkbox:SetPoint("TOPLEFT", BloodlustMusic.songpanel, "TOPLEFT", 290, ((a-31)* -18.6))
+				checkbox:SetSize(27, 27)
+				checkbox.text:SetFontObject("GameFontNormal")
+				checkbox.text:SetText(BloodlustMusic.songNameTable[a])
+				checkbox.text:SetTextColor(1, 1, 1, 1)
+
+				if (getglobal("TestCheckButton "..a):GetChecked() ~= BloodlustMusicSongEnabledTable[a]) then
+					--print("inside getglobal");
+					getglobal("TestCheckButton "..a):SetChecked(BloodlustMusicSongEnabledTable[a]);
+				end
+
+				checkbox:SetScript("OnClick", function(self,event,arg1)
+					if (self:GetChecked()) then
+						--print("set to true");
+						BloodlustMusicSongEnabledTable[a] = true;
+					else
+						--print("set to false");
+						BloodlustMusicSongEnabledTable[a] = false;
+					end
+				end)
+			end
+		end
+	end
+
+
+--What to do on Login, Reload or Zoning
+local Loading_EventFrame = CreateFrame("Frame")
+Loading_EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+Loading_EventFrame:SetScript("OnEvent",
+	function(self, event, isInitialLogin, isReloadingUi)
+			print(BloodlustVolumecache)
+			print("Initial login")
+			print(BloodlustSoundhandle .. " stop song reload")
+			StopSound(BloodlustSoundhandle)
+			SetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber], BloodlustVolumecache)
+			f:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+			C_Timer.After(1, function()
+				f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+			end)
+	end)
+
+--What to do on Logout
+local Logout_EventFrame = CreateFrame("Frame")
+Logout_EventFrame:RegisterEvent("PLAYER_LOGOUT")
+Logout_EventFrame:SetScript("OnEvent",
+	function(self, event, ...)
+	SetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber], BloodlustVolumecache)
+	StopSound(BloodlustSoundhandle)
+	end)
+
+--What to do when Addon loads
+local BloodlustStartingFrame = CreateFrame("FRAME", "BloodlustMusic"); -- Need a frame to respond to events
+BloodlustStartingFrame:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
+BloodlustStartingFrame:RegisterEvent("PLAYER_LOGOUT"); -- Fired when about to log out
+local function BloodlustStartingFrame_OnEvent(self, event, ...)
+	if (event == "ADDON_LOADED") and (... == "BloodlustMusic") then
+		print("Bloodlust Music Ready");
+		-- Our saved variables are ready at this point. If there are none, both variables will set to nil.
+		if (not BloodlustMusicSongEnabledTable) then
+			BloodlustMusicSongEnabledTable = { };
+
+			for a, c in ipairs(BloodlustMusic.songPathTable) do
+				BloodlustMusicSongEnabledTable [a] = true
+			end
+		end
+
+		if (not BloodlustSoundchannelNumber) then
+			BloodlustSoundchannelNumber = 5
+		end
+
+		if(not BloodlustVolumecache) then
+			BloodlustVolumecache = tonumber(GetCVar(BloodlustMusic.soundVolumeTable[BloodlustSoundchannelNumber]))
+		end
+
+		if (not BloodlustSoundhandle) then
+			BloodlustSoundhandle = 0
+		end
+
+		if(not BloodlustChannelVolume) then
+			BloodlustChannelVolume = 1
+		end
+
+		PanelCreation()
+	end
+end
+
+BloodlustStartingFrame:SetScript("OnEvent", BloodlustStartingFrame_OnEvent)
+
+
+
---
+++ BloodlustMusicConfig.lua
@@ -0,0 +1,159 @@
+---------------------------------------------------
+--DO NOT TOUCH BELOW THIS LINE
+---------------------------------------------------
+BloodlustMusic = { };
+
+BloodlustMusic.songPathTable = {
+    [[GasHero.ogg]],        --1
+    [[90sHero.ogg]],        --2
+    [[BrainPowerHero.ogg]], --3 
+    [[BirdHero.ogg]],       --4
+    [[SandHero.ogg]],       --5
+    [[NightHero.ogg]],      --6
+    [[ElephantHero.ogg]],   --7
+    [[SaiyanHero.ogg]],     --8
+    [[JellyfishHero.ogg]],  --9
+    [[TopHero.ogg]],        --10
+    [[DDDHero.ogg]],        --11
+    [[Up&DanceHero.ogg]],   --12
+    [[HardcoreHero.ogg]],   --13
+    [[PerfectHero.ogg]],    --14
+    [[SpeedyHero.ogg]],     --15
+    [[CascadaHero.ogg]],    --16
+    [[SunRainHero.ogg]],    --17
+    [[DontStopHero.ogg]],   --18
+    [[InvadersHero.ogg]],   --19
+    [[CallOnMeHero.ogg]],   --20
+    [[KissKillHero.ogg]],   --21
+    [[LookaHero.ogg]],      --22
+    [[AnchorHero.ogg]],     --23
+    [[FullMetalHero.ogg]],  --24
+    [[KickstartHero.ogg]],  --25
+    [[RiderHero.ogg]],      --26
+    [[RisingSunHero.ogg]],  --27
+    [[BloodSugarHero.ogg]], --28
+    [[DejaVuHero.ogg]],     --29
+    [[RumbleHero.ogg]],     --30
+    [[HurricaneHero.ogg]],  --31
+    [[DontTurnHero1.ogg]],  --32
+    [[DontTurnHero2.ogg]],  --33
+    [[EuronightHero.ogg]],  --34
+    [[BeatinWildHero.ogg]], --35
+    [[KingHero.ogg]],       --36
+    [[SuperRiderHero.ogg]], --37
+    [[PistonGoHero.ogg]],   --38
+    [[DancingHero.ogg]],    --39
+    [[BeatCrazyHero.ogg]],  --40
+    [[SpitfireHero.ogg]],   --41
+    [[ShockOutHero.ogg]],   --42
+    [[SpeedLoverHero.ogg]]  --43
+}
+
+BloodlustMusic.songNameTable = {
+    [[Manuel - GAS GAS GAS]],
+    [[Max Coveri - Running in the 90's]],
+    [[Noma - Brainpower]],
+    [[Lynyrd Skynyrd - Freebird]],
+    [[Darude - Sandstorm]],
+    [[Niko - Night of Fire]],
+    [[The Elephant Rave]],
+    [[Fartwad - Stereo Saiyan 3D]],
+    [[The Jellyfish Jam]],
+    [[Ken Blast - The Top]],
+    [[Mega NRG Man - DDD Initial D]],
+    [[Lou Master - Up & Dance, Up & Go]],
+    [[Fastway - Rockin' Hardcore]],
+    [[Chris Stanton - A Perfect Hero]],
+    [[Marco Polo - Speedy Speed Boy]],
+    [[Cascada - Everytime we Touch]],
+    [[Manuel - Sun In The Rain]],
+    [[Lou Grant - Don't Stop The Music]],
+    [[The Prodigy - Invaders must die]],
+    [[Eric Prydz - Call On Me]],
+    [[Odyssey - Kiss Me Kill Me]],
+    [[Go2 - Looka Bomba]],
+    [[ALESTORM - Fucked With An Anchor]],
+    [[Daniel - Full Metal Cars]],
+    [[Mötley Crüe - Kickstart my Heart]],
+    [[Ace - Rider Of The Sky]],
+    [[Dave Rodgers - Beat Of The Rising Sun]],
+    [[Pendulum - Blood Sugar]],
+    [[Dave Rodgers - Deja Vu]],
+    [[Jock Jams - Let's Get Ready To Rumble]],
+    [[Gold-Rake - Hurricane Man]],
+    [[Go2 - Don't Turn It Off (Chorus)]],
+    [[Go2 - Don't Turn It Off (Solo)]],
+    [[Eurogroove - Euronight]],
+    [[Fastway - Rock Beatin' Wild]],
+    [[Jordan - King Of Eurobeat]],
+    [[Mark Astley - Super Rider]],
+    [[Gordon Jim - Piston Go]],
+    [[Vicky Vale - Dancing]],
+    [[Fastway - Go Beat Crazy]],
+    [[Go2 - Spitfire]],
+    [[Fastway - Shock Out]],
+    [[Speedman - Speed Lover]]
+}
+
+BloodlustMusic.songEnabledTable = {
+    "gas",
+    "nineties",
+    "brain",
+    "freebird",
+    "sandstorm",
+    "night",
+    "elephant",
+    "saiyan",
+    "jellyfish",
+    "thetop",
+    "ddd",
+    "updance",
+    "hardcore",
+    "perfect",
+    "speedy",
+    "cascada",
+    "sunrain",
+    "dontstop",
+    "invaders",
+    "callonme",
+    "kisskill",
+    "looka",
+    "anchor",
+    "fullmetal",
+    "kickstart",
+    "rider",
+    "risingsun",
+    "bloodsugar",
+    "dejavu",
+    "rumble",
+    "hurricane",
+    "dontturn1",
+    "dontturn2",
+    "euronight",
+    "beatinwild",
+    "king",
+    "superrider",
+    "pistongo",
+    "dancing",
+    "beatcrazy",
+    "spitfire",
+    "shockout",
+    "speedlover"
+}
+
+BloodlustMusic.soundChannelTable = {
+    "master",
+    "sfx",
+    "music",
+    "ambience",
+    "dialog"
+}
+BloodlustMusic.soundVolumeTable = {
+    "Sound_MasterVolume",
+    "Sound_SFXVolume",
+    "Sound_MusicVolume",
+    "Sound_AmbienceVolume",
+    "Sound_DialogVolume"
+    
+    
+}
