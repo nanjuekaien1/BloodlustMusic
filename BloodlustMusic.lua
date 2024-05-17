bf27ef02b7d420361cda00f98ac9e51a9bf3274d
Jordi Bottema <jordi.bottema@gmail.com>
2024-05-17 20:40:10 +0200

    -Updated for WoW Patch 10.2.7 -Added Timeless Drums hero for Panda Remix event

--- BloodlustMusic.lua
+++ BloodlustMusic.lua
@@ -129,7 +129,7 @@ local currentFilePath = " "
 local currentlyPlaying = " "
 local minute = 0
 local songNumber = 0
-local spellIDS = {80353, 32182, 2825, 264667, 146555, 178207, 256740, 230935, 309658, 350249, 368245, 390386, 381301, 386540}
+local spellIDS = {80353, 32182, 2825, 264667, 146555, 178207, 256740, 230935, 309658, 350249, 368245, 390386, 381301, 386540, 441076}
 --342242 not implemented yet
 
 C_Timer.After(.1, function() -- wait a bit
